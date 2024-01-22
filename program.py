3d5756a5370076edd9a5b5a41e686dcb89bb0223
Farhan Momin <farhan.m.nafis2412@gmail.com>
2024-01-22 21:20:19 +1100

    Fixed a little thing

--- program.py
+++ program.py
@@ -13,13 +13,21 @@ site_url = "https://taylorswift.ticketek.com.au/"
 driver.get(site_url)
 
 try:
-    button_element_x = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "id_of_element_x")))
+    while True:
+        try:
+            button_element_x = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.ID, "syd-frontier-btn")))
+            button_element_x.click()
+            break
+        except:
+            continue
 
-    button_element_x.click()
-
-    response_element_y = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.ID, "id_of_element_y")))
-
-    response_element_y.click()
+    while True:
+        try:
+            response_element_y = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.ID, "id_of_element_y")))
+            response_element_y.click()
+            break
+        except:
+            continue
 
 except Exception as e:
     print(f"An error occurred: {str(e)}")
